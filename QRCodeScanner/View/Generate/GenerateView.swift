6a76a6ca727c713ed482ed87c5363be247468c12
Alican Karayelli <alicankarayelli@gmail.com>
2020-09-14 14:05:27 +0300

    Present Option to Generate From Clipboard

--- QRCodeScanner/View/Generate/GenerateView.swift
+++ QRCodeScanner/View/Generate/GenerateView.swift
@@ -13,10 +13,10 @@ import LinkPresentation
 struct GenerateView: View {
     
     @State private var data = ""
-    @State private var uiImage: UIImage? = nil
-    @State private var image: Image?
     @State private var showShareSheet = false
-
+    @State private var showingAlert = false
+    @State private var alertMsg = ""
+    @State private var clipboardValue = ""
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -49,7 +49,7 @@ struct GenerateView: View {
                     .padding(.horizontal)
                 
                 
-
+                
                 Image(uiImage: self.generateQRCode(from: self.data))
                     .interpolation(.none)
                     .resizable()
@@ -57,21 +57,52 @@ struct GenerateView: View {
                     .frame(width: 200 ,height: 200)
                     .padding(EdgeInsets(top: 30, leading: 0, bottom: 0, trailing: 0))
                     .onTapGesture {
+                        //Trigger to show share action sheet
                         self.showShareSheet = true
                         
                 }
                 .sheet(isPresented: self.$showShareSheet) {
+                    //Show action sheet
+                    //TODO: Find better solution to not generate QR code second times!!!
                     ShareSheet(photo: self.generateQRCode(from: self.data))
                 }
                 
                 
-
                 Spacer()
+                    
+                    .onAppear{
+                        
+                        //Check if any data exists on clipboard
+                        if let value = UIPasteboard.general.string {
+                            self.clipboardValue = value
+                            self.alertMsg = "view.generator.info.clipboard.part1".localized + value + "view.generator.info.clipboard.part2".localized
+                            self.showingAlert = true
+                        }
+                        
+                        
+                }
+                .alert(isPresented: $showingAlert){
+                    
+                    Alert(title: Text("view.alert.title.info"), message: Text(self.alertMsg), primaryButton: .destructive(Text("generate")) {
+                        self.data = self.clipboardValue
+                        }, secondaryButton: .destructive(Text("no")))
+                }
                 
             }
             .navigationBarTitle("view.generator.button.generate")
         }
     }
+    
+    
+    
+    
+    /**
+     Generates QR code with given string
+     
+     - Parameter string: String data to generate QR.
+     
+     - Returns: UIImage.
+     */
     
     func generateQRCode(from string: String) -> UIImage {
         let data = Data(string.utf8)
